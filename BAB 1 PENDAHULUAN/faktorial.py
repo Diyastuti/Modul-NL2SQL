1702517274fa934863b272608cc791a682ec9373
Dianita Aira Diyastuti <nitadian365@gmail.com>
2026-05-12 21:32:47 +0700

    Delete BAB 1 PENDAHULUAN/faktorial.py

--- BAB 1 PENDAHULUAN/faktorial.py
+++
@@ -1,8 +0,0 @@
-def faktorial(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * faktorial(n - 1)
-
-# ini bagian input dari pengguna
-n = int(input("Masukkan angka untuk dihitung faktorialnya: "))
-print("Faktorial dari", n, "adalah", faktorial(n))
